9b64851d03a78f5d73ef2714aeb0d8bcf024ee0f
an_c23 <aracelicrrll@gmail.com>
2016-05-18 21:21:50 +0000

    color-changer

--- solution.js
+++ solution.js
@@ -1,1 +1,15 @@
-
+/*global $*/
+$( document ).ready(function() {
+var colors= ["blue", "purple", "green", "yellow", "red"]
+function changecolor(){
+var randomIndex= Math.floor(Math.random()*colors.length);
+$("body").css("background-color", colors[randomIndex]);
+}
+$("#change").click(changecolor);
+function addcolor(){
+    var newcolor = $("#newcolor").val();
+    colors.push(newcolor);
+    $("body").css("background-color", colors[colors.length - 1])
+}
+    $("#add").click(addcolor);
+});
